c5c46004b01c1e511421969ddb83c6552d15a6d2
generalov <alexander.generalov1987@gmail.com>
2024-04-29 15:27:27 +0300

    all 4 tasks done

--- student_class.py
+++ student_class.py
@@ -18,7 +18,7 @@ class Student:
             else:
                 lecturer.grades[course] = [grade]
     
-    def aver_grade(self):
+    def aver_grade(self): # calculae average grade on all courses
         total_grade = 0
         if self.grades:
             grade_amount = 0
@@ -46,7 +46,7 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
     
-    def aver_grade(self):
+    def aver_grade(self): # calculae average grade on all courses
         total_grade = 0
         if self.grades:
             grade_amount = 0
@@ -76,11 +76,37 @@ class Reviewer(Mentor):
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}"
 
-def best_student(students):
-    return max(students, key=students.get)
+def best_student(students): # returns name of a student with highest average grade on all courses
+    grade = 0
+    name = ""
+    for student in students:
+        if student.aver_grade() >= grade:
+            grade = student.aver_grade()
+            name = student.name
+    return name
     
-def best_lecturer(lecturers):
-    return max(lecturers, key=lecturers.get)
+def best_lecturer(lecturers): # returns name of a lecturer with highest average grade on all courses
+    grade = 0
+    name = ""
+    for lecturer in lecturers:
+        if lecturer.aver_grade() >= grade:
+            grade = lecturer.aver_grade()
+            name = lecturer.name
+    return name
+
+def aver_grade_of_all_students(students, course): # returns avarage grade of all students on dedicated course
+    res = 0
+    for student in students:
+        if course in student.courses_in_progress:
+            res += sum(student.grades[course]) / len(student.grades[course])
+    return round(res / len(students), 1)
+    
+def aver_grade_of_all_lecturers(students, course): # returns avarage grade of all lecturers on dedicated course
+    res = 0
+    for lecturer in lecturers:
+        if course in lecturer.courses_attached:
+            res += sum(lecturer.grades[course]) / len(lecturer.grades[course])
+    return round(res / len(lecturers), 1)    
 
 student1 = Student('Ruoy', 'Eman', 'your_gender')
 student1.courses_in_progress += ['Python']
@@ -88,35 +114,46 @@ student1.courses_in_progress += ['Git']
 student1.finished_courses += ['Введение в программирование']
 student2 = Student('Mike', 'Jugger', 'your_gender')
 student2.courses_in_progress += ['C++']
-student2.courses_in_progress += ['Git']
+student2.courses_in_progress += ['Python']
 student2.finished_courses += ['Введение в программирование']
 
+mentor1 = Mentor("Lary", "Hofman")
+mentor2 = Mentor("Kily", "Monogue")
 
 reviewer1 = Reviewer('Some', 'Buddy')
 reviewer1.courses_attached += ['Python']
 reviewer2 = Reviewer('Teddy', 'Bear')
 reviewer2.courses_attached += ['Git']
+reviewer2.courses_attached += ['Python']
 
 lecturer1 = Lecturer("Ricky", "Martin")
 lecturer1.courses_attached += ["Python"]
 lecturer2 = Lecturer("Too", "Tired")
 lecturer2.courses_attached += ["C++"]
+lecturer2.courses_attached += ["Python"]
 
 reviewer1.rate_hw(student1, 'Python', 10)
 reviewer1.rate_hw(student1, 'Python', 10)
 reviewer1.rate_hw(student1, 'Python', 8)
+reviewer2.rate_hw(student2, 'Python', 7)
+reviewer2.rate_hw(student2, 'Python', 10)
+reviewer2.rate_hw(student2, 'Python', 7)
+
 
 student1.rate_lecturer(lecturer1, "Python", 7)
 student1.rate_lecturer(lecturer1, "Python", 9)
-student2.rate_lecturer(lecturer2, "C++", 10)
-student2.rate_lecturer(lecturer2, "C++", 9)
+student2.rate_lecturer(lecturer2, "Python", 10)
+student2.rate_lecturer(lecturer2, "Python", 8)
 
 
-students = {student1.name : student1.aver_grade(), student2.name : student2.aver_grade()}
-lecturers = {lecturer1.name : lecturer1.aver_grade(), lecturer2.name : lecturer2.aver_grade()}
+students = [student1, student2]
+lecturers = [lecturer1, lecturer2]
 
 print(reviewer1)
 print(lecturer1)
 print(student1)
 print(f"Лучший студент: {best_student(students)}")
 print(f"Лучший лектор: {best_lecturer(lecturers)}")
+print(f"Средний бал всех студентов на курсе Python: {aver_grade_of_all_students(students, 'Python')}")
+print(f"Средний бал всех лекторов на курсе Python: {aver_grade_of_all_lecturers(lecturers, 'Python')}")
+
